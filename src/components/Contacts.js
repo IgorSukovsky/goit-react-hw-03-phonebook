6cbbc525ab49de8fc8e89a63305df9c4347e2d38
IgorSukovsky <Sukovsky70@gmail.com>
2021-06-15 18:28:32 +0300

    add localStorage

---
+++ src/components/Contacts.js
@@ -0,0 +1,90 @@
+import { Component } from "react";
+import { v4 as uuid } from "uuid";
+import ContactForm from "./ContactForm/ContactForm";
+import Filter from "./Filter/Filter";
+import ContactsList from "./ContactList/ContactList";
+import styles from "./Contacts.module.css";
+
+class Contacts extends Component {
+  state = {
+    items: [],
+    filter: "",
+  };
+  handleDelete = (id) => {
+    this.setState((prevState) => ({
+      items: prevState.items.filter((item) => item.id !== id),
+    }));
+  };
+
+  handleChange = (evt) => {
+    this.setState({ [evt.target.name]: evt.target.value });
+  };
+
+  handleSubmit = (term) => {
+    if (!term) {
+      alert("Поле не может быть пустым!");
+      return;
+    }
+
+    const isDuplicate = this.state.items.some(
+      (item) => item.name === term.name
+    );
+    if (isDuplicate) {
+      alert("Контакт: " + term.name + " уже существует ");
+      return;
+    }
+
+    const newTodo = {
+      id: uuid(),
+      name: term.name,
+      number: term.number,
+    };
+
+    this.setState((prevState) => {
+      const newItems = [newTodo, ...prevState.items];
+      return { items: newItems };
+    });
+  };
+
+  // =========================
+  componentDidMount() {
+    console.log("[componentDidMount]");
+    const items = localStorage.getItem("items");
+    if (items) {
+      const parsedItems = JSON.parse(items);
+      this.setState({ items: parsedItems });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("[componentDidUpdate]");
+    if (prevState.items !== this.state.items) {
+      localStorage.setItem("items", JSON.stringify(this.state.items));
+    }
+  }
+
+  render() {
+    const { items, filter } = this.state;
+    const formattedFilter = filter.toLowerCase().trim();
+    const filteredItems = items.filter((item) =>
+      item.name.toLowerCase().includes(formattedFilter)
+    );
+
+    return (
+      <div className="container">
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm handleSubmit={this.handleSubmit} />
+        <h2 className={styles.titleContact}>Contacts</h2>
+        <div className={styles.block}>
+          <Filter filter={filter} handleChange={this.handleChange} />
+          <ContactsList
+            items={filteredItems}
+            handleDelete={this.handleDelete}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contacts;
